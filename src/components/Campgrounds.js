a1a9b3e677f65c60c3a75f67fb5365af3f272404
Sheila <rugut.cheruto@student.moringaschool.com>
2023-02-13 15:03:14 +0300

    Fetch data from json rest API

--- src/components/Campgrounds.js
+++ src/components/Campgrounds.js
@@ -1,38 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import "./Campgrounds.css";
 
 function Campgrounds() {
-  return <div>Campgrounds</div>;
+  const [campground, setCampground] = useState([])
+
+  // get campgrounds from API
+  useEffect(() => {
+    fetch(' http://localhost:5000/campgrounds')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setCampground(data)
+      })
+  }, [])
+  return (
+    <div>
+      
+       
+      
+      <div className="filter">
+        
+          <label htmlFor="location">Location:</label>
+          <select
+            id="location"
+            name="location"
+            // value={filter.location}
+            // onChange={handleChange}
+          >
+            <option value="">All</option>
+            <option value="North">North</option>
+            <option value="South">South</option>
+            <option value="East">East</option>
+            <option value="West">West</option>
+          </select>
+          <label htmlFor="location">Location:</label>
+          <select
+            id="location"
+            name="location"
+            // value={filter.location}
+            // onChange={handleChange}
+          >
+            <option value="">All</option>
+            <option value="North">North</option>
+            <option value="South">South</option>
+            <option value="East">East</option>
+            <option value="West">West</option>
+          </select>
+       
+      </div>
+     
+      <div className="destination-container">
+        {/* <h2 className="section-title">Featured Destinations</h2>
+        <p className="section-description">
+          Check out some of our most popular camping spots
+        </p> */}
+ {campground.map((campground) =>
+        <div className="destination-cards">
+          <div className="destination-card">
+            <img
+              className="destination-card__image"
+              src={campground.image_url}
+              alt=" Aberdare National Park"
+            />
+            <h3 className="destination-card__title">{campground.name}</h3>
+            <p className="destination-card__description">
+             {campground.description}
+            </p>
+            <button className="destination-card__btn">Learn More</button>
+          </div>
+          {/* <div className="destination-card">
+            <img
+              className="destination-card__image"
+              src="https://uzamart.com/wp-content/uploads/2021/01/Camp-Carnellys-1024x683.jpg"
+              alt="Camp Carnellys Lake Naivasha"
+            />
+            <h3 className="destination-card__title">
+              {campground.name}
+            </h3>
+            <p className="destination-card__description">
+              Enjoy a tranquil and scenic camping experience with breathtaking
+              views of Lake Naivasha and stunning sunsets in a serene and
+              peaceful environment.
+            </p>
+            <button className="destination-card__btn">Learn More</button>
+          </div> */}
+          {/* <div className="destination-card">
+            <img
+              className="destination-card__image"
+              src="https://images.unsplash.com/photo-1571636240366-1606c4a13f55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="Shipton's Camp Ground"
+            />
+            <h3 className="destination-card__title">Shipton's Camp Ground</h3>
+            <p className="destination-card__description">
+              Take in the lush, scenic beauty of Mount Kenya on your next
+              camping trip.
+            </p>
+            <button className="destination-card__btn">Learn More</button>
+          </div> */}
+        </div>
+        )}
+      </div>
+      
+    </div>
+  );
 }
 
 export default Campgrounds;
-// //  function twoSum(arr, target) {
-// //   for(let i = 0; i < arr.length; i++){
-// //       for(let j = i+1; j < arr.length; j++){
-// //           if(arr[i] + arr[j] === target){
-// //               return [i, j]
-// //           }
-// //           else
-// //           {return null;}
-// //       }
-// //   }
-// // };
 
-// // console.log(twoSum([2,7,11,15],9))
-// // let array = [2, 7, 11, 15];
-
-// let target = 9;
-
-// function solution(array, target) {
-//   for (let i = 0; i < array.length; i++) {
-//     let val = target - array[i];
-
-//     if (i === array.indexOf(val)) {
-//       continue;
-//     }
-
-//     if (array.includes(val)) {
-//       return [array.indexOf(val), i];
-//     }
-//   }
-//   return null;
-// }
